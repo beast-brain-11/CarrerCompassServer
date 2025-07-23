0bd4d78264e4d105abdfbbec309964be96455b32
priaanshgupta <priaanshgupta@gmail.com>
2025-07-23 14:23:12 +0000

    update 5

--- src/config/db.ts
+++ src/config/db.ts
@@ -10,4 +10,19 @@ const pool = new Pool({
   },
 });
 
+pool.connect((err, client, release) => {
+  if (err) {
+    return console.error('Error acquiring client', err.stack);
+  }
+  if (client) {
+    client.query('SELECT NOW()', (err, result) => {
+      release();
+      if (err) {
+        return console.error('Error executing query', err.stack);
+      }
+      console.log('Database connected successfully');
+    });
+  }
+});
+
 export default pool;

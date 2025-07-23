64d54f69e9ca8df9dc8901e134700d35b4009ca1
priaanshgupta <priaanshgupta@gmail.com>
2025-07-23 13:18:49 +0000

    update

--- src/index.ts
+++ src/index.ts
@@ -21,6 +21,4 @@ app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
 
-app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
-});
+export default app;
